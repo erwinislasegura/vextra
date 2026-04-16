553e322a0e7db0e52ee8d4e7b31157d682614a74
erwinislasegura <erwin.2785@gmail.com>
2026-04-16 17:12:09 -0400

    Quitar tarjeta de estado de productos y subir bloque KPI

--- aplicacion/controladores/Empresa/PanelEmpresaControlador.php
+++ aplicacion/controladores/Empresa/PanelEmpresaControlador.php
@@ -26,7 +26,6 @@ class PanelEmpresaControlador extends Controlador
         $soporteChatModel = new SoporteChat();
         $planEmpresa = (new Plan())->obtenerPlanActivoEmpresa($empresaId);
         $resumenComercial = $gestionComercialModel->estadisticasInicio($empresaId);
-        $productosInventario = $inventarioModel->listarProductos($empresaId);
         $ordenesCompra = $inventarioModel->listarOrdenesCompra($empresaId);
         $ventasPos = $puntoVentaModel->listarVentas($empresaId);
         $seguimientos = $gestionComercialModel->listarSeguimientoCotizaciones($empresaId, '', '', 200);
@@ -36,19 +35,10 @@ class PanelEmpresaControlador extends Controlador
         $chatsSoporte = $soporteChatModel->listarChatsEmpresa($empresaId, 5);
         $chatsSoporteNoLeidos = $soporteChatModel->contarNoLeidosEmpresa($empresaId);
 
-        $stockBajo = 0;
-        $stockCritico = 0;
-        foreach ($productosInventario as $producto) {
-            $stockActual = (float) ($producto['stock_actual'] ?? 0);
-            $stockMinimo = (float) ($producto['stock_minimo'] ?? 0);
-            $stockCriticoRef = (float) ($producto['stock_critico'] ?? 0);
-            if ($stockMinimo > 0 && $stockActual <= $stockMinimo) {
-                $stockBajo++;
-            }
-            if ($stockCriticoRef > 0 && $stockActual <= $stockCriticoRef) {
-                $stockCritico++;
-            }
-        }
+        $resumenStock = $productoModel->contarPorEstadoStock($empresaId);
+        $stockCritico = (int) ($resumenStock['critico'] ?? 0);
+        $stockBajo = (int) ($resumenStock['bajo'] ?? 0);
+        $stockNormal = (int) ($resumenStock['normal'] ?? 0);
 
         $ordenesPendientes = 0;
         foreach ($ordenesCompra as $ordenCompra) {
@@ -101,6 +91,7 @@ class PanelEmpresaControlador extends Controlador
             'dias_restantes_plan' => isset($planEmpresa['fecha_vencimiento']) ? (int) floor((strtotime((string) $planEmpresa['fecha_vencimiento']) - strtotime(date('Y-m-d'))) / 86400) : null,
             'stock_bajo' => $stockBajo,
             'stock_critico' => $stockCritico,
+            'stock_normal' => $stockNormal,
             'ordenes_compra_pendientes' => $ordenesPendientes,
             'ventas_hoy' => $ventasHoy,
             'monto_ventas_hoy' => $montoVentasHoy,
